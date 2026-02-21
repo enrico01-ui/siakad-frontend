79c00bfe391c219ba3409a43bdc81f51a3705e4c
Emanuel Enrico Anindya Wibawa <220711890@students.uajy.ac.id>
2026-02-21 20:03:35 +0700

    null check

--- src/routes/RoleRoute.jsx
+++ src/routes/RoleRoute.jsx
@@ -3,7 +3,8 @@ import { getUser } from "../services/auth";
 
 export default function RoleRoute({ allow }) {
   const user = getUser();
-
+  if(user && user.role) console.log("User role:", user.role.nama);
+  
   if (!user) return <Navigate to="/" replace />;
 
   return allow.includes(user.role.nama)
--- src/services/auth.jsx
+++ src/services/auth.jsx
@@ -3,6 +3,7 @@ export const getToken = () => localStorage.getItem("token");
 
 export const getUser = () => {
   const user = localStorage.getItem("user");
+  
   return user ? JSON.parse(user) : null;
 };
 
